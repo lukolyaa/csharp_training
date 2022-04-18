4c398349fa119c6f1c4ef0a3748a8f9c9585562a
Olga Raitina <olga.raitina@icl-services.com>
2022-04-18 20:30:05 +0300

    Release addressbook_tests_autoit + 18 task

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupRemovalTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace autoit_addressbook
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData removedGroup = oldGroups[0];
+            app.Groups.Remove(removedGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            oldGroups.RemoveAt(0);
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
